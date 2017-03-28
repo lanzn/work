0937e62d1db79454b5a5b3986c87c9ebd6865318
lanzn <741144531@qq.com>
2017-03-28 18:55:29 +0800

    爬虫的代码
    爬取豆瓣电影 爬取信息包括电影名称、电影内容简介、电影详情页链接，并保存到mysql中

---
+++ work2.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import requests
+from bs4 import BeautifulSoup
+baseUrl = "https://movie.douban.com/top250?start=%d&filter="
+def get_movies(start):
+    url = baseUrl % start
+    lists = []
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    items = soup.find("ol", "grid_view").find_all("li",limit=2)
+    for i in items:
+        movie = {}
+        movie["rank"] = i.find("em").text
+        movie["link"] = i.find("div","pic").find("a").get("href")
+        movie["name"] = i.find("span", "title").text
+        nexturl=movie["link"]
+        print(nexturl)
+        nexthtml=requests.get(nexturl)
+        nextsoup = BeautifulSoup(nexthtml.content, "html.parser")
+        movie["body"]=nextsoup.find("div","related-info").find("span","all hidden").text if(nextsoup.find("div","related-info").find("span","all hidden")) else nextsoup.find("div","related-info").find("span",attrs={"property":"v:summary"}).text
+        #print(movie["body"])
+        #movie["body"]=""
+        lists.append(movie)
+    return lists
+
+if __name__ == "__main__":
+    db = mysql.connector.connect(host="localhost",user="root",password="root",db="test",charset="utf8mb4")
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS movies")
+    createTab = """CREATE TABLE movies(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        body VARCHAR (500) CHARACTER SET utf8 COLLATE utf8_general_ci ,
+        rank VARCHAR(4) NOT NULL,
+        link VARCHAR(50) NOT NULL
+    )"""
+    cursor.execute(createTab)
+    start = 0
+    #while (start < 250):
+    lists = get_movies(start)
+    for i in lists:
+        sql = "INSERT INTO `movies`(`name`,`body`,`rank`,`link`) VALUES(%s,%s,%s,%s)"
+        try:
+            cursor.execute(sql, (i["name"],i["body"], i["rank"], i["link"]))
+            db.commit()
+            print(i["name"]+" is success")
+        except:
+            db.rollback()
+        #start += 25
+    db.close()
